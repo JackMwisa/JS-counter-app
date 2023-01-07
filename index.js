98ee3892de577a11f4b6e6c0d46565fd0700beea
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-01-07 14:52:22 +0300

    increment button style

--- index.js
+++ index.js
@@ -22,13 +22,16 @@
 // count = count + 8
 // console.log(count)
 
-let bonusPoints = 50
+// let bonusPoints = 50
 
-bonusPoints = bonusPoints + 100
-console.log(bonusPoints)
+// bonusPoints = bonusPoints + 100
+// console.log(bonusPoints)
 
-bonusPoints = bonusPoints - 25
-console.log(bonusPoints)
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
 
-bonusPoints = bonusPoints + 70
-console.log(bonusPoints)
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+
+
