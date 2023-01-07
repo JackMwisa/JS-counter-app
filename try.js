5ed719f7e95c1589c3882495a4899104657da00a
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-01-07 16:51:53 +0300

    Update try.js

--- try.js
+++ try.js
@@ -1,6 +1,5 @@
+let name = 'jack '
+let greeting = 'hello, my name is '
+let myGreeting = greeting + name
 
-let user = "Jack"
-
-let message = 'You have 3 new notifications'
-
-console.log(message + ", "+ user + "!")
+console.log(myGreeting)
