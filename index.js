fd8aa9705078a600b7a26cbcfa755ec70bbb6c5b
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-01-07 14:33:05 +0300

    javascript

---
+++ index.js
@@ -0,0 +1,19 @@
+// document.getElementById("count-el").innerText = 78
+
+
+
+// Comment Ctrl + k + c
+
+// let count = 0
+
+// myAge = 23
+
+// console.log(myAge)
+
+/////////////////////////////
+//
+myAge = 22
+humanDogRatio = 7
+myDogsAge = myAge * humanDogRatio
+
+console.log(myDogsAge)
