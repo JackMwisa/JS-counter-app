d6866f9265041c962e5a688707d7dc11a01182d6
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-01-07 17:56:33 +0300

    Update try.js

--- try.js
+++ try.js
@@ -3,3 +3,12 @@ let greeting = 'hello, my name is '
 let myGreeting = greeting + name
 
 console.log(myGreeting)
+
+
+// // let points = 4
+
+// let bpoint = "10"
+
+// let totalPoints = points + bpoint
+
+// console.log(totalPoints)
