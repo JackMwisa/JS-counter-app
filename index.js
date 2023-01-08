ce778dc4af646d2d5d277d114c777d802af031fb
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-01-08 20:59:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 
-
-////////////////////////////////////////////////////////
-
-// let countEl = document.getElementById("count-el");
-
-// console.log(countEl);
-
-// let count = 0;
-
-// function increment() {
-//   count = count + 1;
-//   countEl.innerText = count;
-// }
-
-///////////////////////////////////////////////////////
 let saveEl = document.getElementById("save-el")
 
 let counter = document.getElementById("count-el");
